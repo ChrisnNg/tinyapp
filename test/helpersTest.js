5c028041d15070a2b39bac8dd98efd9c10af6b11
ChrisnNg <cristopherng@hotmail.com>
2019-09-05 20:20:11 +0000

    added tests for urlsForUser

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -12,6 +12,11 @@ const testUsers = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
+};
+
+const urlDatabase = {
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: 'userRandomID'},
+  "9sm5xK": { longURL: "http://www.google.com", userID: 'user2RandomID'}
 };
 
 describe('getUserByEmail', function() {
@@ -28,4 +33,21 @@ describe('getUserByEmail', function() {
     // Write your assert statement here
     assert(user === expectedOutput, `${user} is not equal to ${expectedOutput}`);
   });
+});
+
+describe('urlsForUser', function() {
+  it('should return an object of urls for the user', function() {
+    const urls = urlsForUser("user2RandomID", urlDatabase);
+    const expectedOutput = {'9sm5xK': "http://www.google.com"};
+    // Write your assert statement here
+    assert.deepEqual(urls, expectedOutput);
+  });
+
+  it('a non-existent user should return an empty object', function() {
+    const urls = urlsForUser("user3RandomID", urlDatabase);
+    const expectedOutput = {};
+    // Write your assert statement here
+    assert.deepEqual(urls, expectedOutput);
+  });
+
 });
